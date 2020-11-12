f488ae440984bf3bd33bb00e84748a48814b5f26
Jeff Van Etten <jvanetten19@gmail.com>
2020-11-12 10:05:01 -0500

    Added in comments for readability

--- client/src/Components/Nav/Hamburger.jsx
+++ client/src/Components/Nav/Hamburger.jsx
@@ -1,7 +1,10 @@
+// imports for the component
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import RightNav from './RightNav';
 
+//This is styled components at its finest... just doing inner CSS,,,
+// below line 23 is where all the animation magic comes in 
 const StyledBurger = styled.div`
   width: 2rem;
   height: 2rem;
@@ -15,6 +18,8 @@ const StyledBurger = styled.div`
     justify-content: space-around;
     flex-flow: column nowrap;
   }
+
+  
   div {
     width: 2rem;
     height: 0.25rem;
@@ -35,6 +40,7 @@ const StyledBurger = styled.div`
   }
 `;
 
+// making compoenet here with => function and useState to trigger the opening on the nav bar
 const Burger = () => {
   const [open, setOpen] = useState(false)
   
@@ -45,6 +51,7 @@ const Burger = () => {
         <div />
         <div />
       </StyledBurger>
+      {/** calling the right side nav bar component */}
       <RightNav open={open}/>
     </>
   )
