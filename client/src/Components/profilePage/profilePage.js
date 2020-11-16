47e49f206d00bc168cccb736797eaee3566b0146
Emily <emilysaber13@gmail.com>
2020-11-16 17:01:13 -0500

    auth/login refresh bug fixed!

--- client/src/Components/profilePage/profilePage.js
+++ client/src/Components/profilePage/profilePage.js
@@ -6,15 +6,23 @@ import firebaseApp from "../auth/firebase.js";
 function ProfilePage() {
   const [profile, setProfile] = useState("");
 
-  const currentUser = localStorage.getItem("newemail");
+  //testing this id stuff now
+  const userId = profile._id;
+  localStorage.setItem("id", userId);
 
+  //call this method on firebase to get email and throw into fetch to retrievd the user info from our database
+  const userEmail = firebaseApp.auth().currentUser.email;
+
+  //logout and redirect to home/ landing page
   const handleLogout = () => {
     firebaseApp.auth().signOut();
     <Redirect to="/" />;
   };
 
+  //grab information for user based on email match in database
+  //this is a route set up in the server
   const fetchUser = async () => {
-    fetch(`/user/${currentUser}`)
+    fetch(`/user/${userEmail}`)
       .then((response) => response.json())
       .then((userProfile) => {
         setProfile(userProfile[0]);
