9263a2b3739ce8f1f92ebd85b77469a5a4c86bb9
Jeff Van Etten <jvanetten19@gmail.com>
2020-11-17 16:32:15 -0500

    added more moblie first css

--- client/src/Components/Nav/RightNav.jsx
+++ client/src/Components/Nav/RightNav.jsx
@@ -46,8 +46,7 @@ const RightNav = ({ open }) => {
       <a href="/" ><li>News</li></a>      
       <a href="/userlogin-page"><li>Log In</li></a>
       <a href="/user"><li>Sign up</li></a>
-      <a href="/event"><li>Add Event</li></a>
-      <a href="/addinfo-page"><li>Add Event Participant</li></a>  
+      <a href="/event"><li>Add Event</li></a>  
       <a href="/map"><li>VT Covid Map</li></a> 
     </Ul>
   );
