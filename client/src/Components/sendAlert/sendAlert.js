0c49a0a3a980b70a4f060a476d38e20f0afa75e3
Emily <emilysaber13@gmail.com>
2020-11-30 12:05:51 -0500

    log out page bug fixed

--- client/src/Components/sendAlert/sendAlert.js
+++ client/src/Components/sendAlert/sendAlert.js
@@ -1,23 +1,37 @@
 import React from "react";
-import './sendAlert.css'
-
-function sendTheAlert() {
-  fetch(`/send-alert`, {method:'POST'});
-  document.location = '/userprofile'
-}
-
-function returnToUserProfile() {
-  document.location = '/userprofile'
-}
-
+import { useHistory } from "react-router-dom";
+import "./sendAlert.css";
 
 function SendAlert() {
-  return <div>
-    <h1 id="sendAlertTitle" >Confirm that you want to send POSITIVE COVID alert?</h1>
-    <button className='sendAlertButton' onClick={sendTheAlert}>Yes, I am positive</button>
-    <br></br>
-    <button className='sendAlertButton' onClick={returnToUserProfile} >Return to Profile</button>
-    </div>;
+  let history = useHistory();
+
+  function sendTheAlert() {
+    fetch(`/send-alert`, { method: "POST" });
+    history.push({
+      pathname: "/userprofile",
+    });
+  }
+
+  function returnToUserProfile() {
+    history.push({
+      pathname: "/userprofile",
+    });
+  }
+
+  return (
+    <div>
+      <h1 id="sendAlertTitle">
+        Confirm that you want to send POSITIVE COVID alert?
+      </h1>
+      <button className="sendAlertButton" onClick={sendTheAlert}>
+        Yes, I am positive
+      </button>
+      <br></br>
+      <button className="sendAlertButton" onClick={returnToUserProfile}>
+        Return to Profile
+      </button>
+    </div>
+  );
 }
 
 export default SendAlert;
